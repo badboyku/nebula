a468fce4e862188f5244ba07da92f9ab663a2346
Joey Atkinson <badboyKU@gmail.com>
2025-08-15 22:19:30 -0700

    Fixing build

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,8 +5,8 @@ module.exports = {
     react: { version: '999.999.999' },
   },
   rules: {
-    'import/no-extraneous-dependencies': 'off',
-    'import/no-unresolved': 'off',
+    // 'import/no-extraneous-dependencies': 'off',
+    // 'import/no-unresolved': 'off',
     'react/react-in-jsx-scope': 'off',
     'react/require-default-props': 'off',
   },
